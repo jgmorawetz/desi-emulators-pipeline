99cdc252196f217b25bcb8aec2f59eac652a31e4
James Morawetz <77302761+jgmorawetz@users.noreply.github.com>
2026-04-30 12:03:21 -0400

    Adjusted pipeline path in data generation script.

--- data_generation.sh
+++ data_generation.sh
@@ -3,7 +3,7 @@
 #SBATCH -C cpu
 #SBATCH -q regular
 #SBATCH --job-name=data_gen
-#SBATCH --time=03:00:00
+#SBATCH --time=08:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=128
 #SBATCH --mem=300G
@@ -12,7 +12,7 @@
 module load julia
 
 export JULIA_DEPOT_PATH=/global/homes/j/jgmorawe/.julia
-export JULIA_PROJECT=/global/homes/j/jgmorawe/emulators_pipeline
+export JULIA_PROJECT=/global/homes/j/jgmorawe/desi-emulators-pipeline
 export JULIA_WORKER_TIMEOUT=600
 export LC_ALL=C
 export LANG=C
